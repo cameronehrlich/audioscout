a6af2855694df985dad0bfab84c375dc2e8d04a8
starkd88@gmail.com <starkd88@gmail.com@e7c55dd1-0a2a-d065-63ff-bbf2db920a18>
2011-02-10 19:32:51 +0000

    update copyright

--- trunk/tblserver_driver.c
+++ trunk/tblserver_driver.c
@@ -1,15 +1,22 @@
 /*
+    Audio Scout - audio content indexing software
+    Copyright (C) 2010  D. Grant Starkweather & Evan Klinger
+    
+    Audio Scout is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    pHash, the open source perceptual hash library
-    Copyright (C) 2010 Aetilius, Inc.
-    All rights reserved.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    D Grant Starkweather - dstarkweather@phash.org
-    Evan Klinger - eklinger@phash.org
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    This is a driver program to test the throughput
-    the table server component.  
-
+    D. Grant Starkweather - dstarkweather@phash.org
+    Evan Klinger          - eklinger@phash.org
 */
 
 #include <stdlib.h>
