b99b89fbd74d2092c40a02acaf44ac30dfcdfe69
starkd88 <starkd88@gmail.com>
2011-02-10 19:32:51 +0000

    update copyright

--- trunk/metadatadb_driver.c
+++ trunk/metadatadb_driver.c
@@ -1,16 +1,22 @@
 /*
+    Audio Scout - audio content indexing software
+    Copyright (C) 2010  D. Grant Starkweather & Evan Klinger
+    
+    Audio Scout is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    pHash, the open source perceptual hash library
-    Copyright (C) 2009 Aetilius, Inc.
-    All rights reserved.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    No Permision is herby granted to redistribute.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    D Grant Starkweather - dstarkweather@phash.org
-    E Klinger            - eklinger@phash.org
-
-    Test driver program for the metadatadb server
-
+    D. Grant Starkweather - dstarkweather@phash.org
+    Evan Klinger          - eklinger@phash.org
 */
 
 #include <stdlib.h>
