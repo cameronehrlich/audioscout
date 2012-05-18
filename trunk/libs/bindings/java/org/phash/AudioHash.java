fdb6fd0b48f857c3c7ce93fbb2ee1c2bca32f1a4
starkd88@gmail.com <starkd88@gmail.com@e7c55dd1-0a2a-d065-63ff-bbf2db920a18>
2012-05-18 18:09:43 +0000

    java bindings

--- trunk/libs/bindings/java/org/phash/AudioHash.java
+++ trunk/libs/bindings/java/org/phash/AudioHash.java
@@ -1,5 +1,13 @@
 package org.phash;
-public class AudioHash extends Hash
-{
-	public int[] hash;	
+
+public class AudioHash {
+
+    public native static float[] readAudio(String filename, int sr, float nbsecs, AudioMetaData mdataObj);
+
+    public native static AudioHashObject audioHash(float[] buf, int P, int sr);
+
+    static {
+	System.loadLibrary("pHashAudio-jni");
+    }
+
 }
